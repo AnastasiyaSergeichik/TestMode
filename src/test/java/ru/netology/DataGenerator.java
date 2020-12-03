f0876f0eec442e0574db1335b05b70ff043bf273
Sergey Sergeychik <sergey.sergeychik@gmail.com>
2020-12-03 14:03:40 +0300

    Корректировка тестов

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -33,7 +33,6 @@ public class DataGenerator {
                 .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
                 .then() // "тогда ожидаем"
                 .statusCode(200); // код 200 OK
-
         return registrationUser;
     }
 }
